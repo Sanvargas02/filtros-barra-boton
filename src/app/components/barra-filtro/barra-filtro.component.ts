6c3ce49554a5a1ed64d8d867ae9e5ed3a74fe430
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-28 17:30:56 -0500

    Los Filtros listos para usar

--- src/app/components/barra-filtro/barra-filtro.component.ts
+++ src/app/components/barra-filtro/barra-filtro.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-barra-filtro',
   templateUrl: './barra-filtro.component.html',
-  styleUrls: ['./barra-filtro.component.css']
+  styleUrls: ['./barra-filtro.component.css'],
 })
 export class BarraFiltroComponent {
-
   //Propiedad para el Pipe en el filtro de texto
   filterPost: string = '';
 
@@ -16,40 +15,52 @@ export class BarraFiltroComponent {
       id: 1,
       title: 'La Carreta',
       date: '02/04/2023',
-      services: ['hotel', 'Hoyo feo']
+      services: ['hotel', 'Hoyo feo'],
+      ciudad: 'neiva',
     },
     {
       id: 2,
       title: 'Paragón Plaza',
       date: '02/04/2022',
-      services: ['Restaurante']
+      services: ['Restaurante'],
+      ciudad: 'garzon',
     },
     {
       id: 3,
       title: 'Carne a la Llanera',
       date: '02/04/2021',
-      services: ['motel', 'Hoyo feo']
+      services: ['motel', 'Hoyo feo'],
+      ciudad: 'gigante',
     },
     {
       id: 4,
       title: 'Otro restaurant',
       date: '02/04/2020',
-      services: ['Hoyo feo', 'Cancha']
+      services: ['Hoyo feo', 'Cancha'],
+      ciudad: 'tarqui',
     },
     {
       id: 5,
       title: 'Ultima búsqueda',
       date: '02/04/2019',
-      services: ['Cancha']
-    }
-  ]
+      services: ['Cancha'],
+      ciudad: 'hobo',
+    },
+    {
+      id: 6,
+      title: 'Ultima búsqueda',
+      date: '02/04/2019',
+      services: ['Cancha'],
+      ciudad: 'hobo',
+    },
+  ];
 
   //Pasamos todo nuestros datos a un arreglo nuevo para trabajarlo e imprimirlo en html
   //Esto para no alterar el arreglo original
   filteredItems: any[] = this.posts;
 
   //Función para filtrar por medio del botón
-  applyFilter(selectedCategory: any) {
+  applyFilterServices(selectedCategory: any) {
     let value = selectedCategory.target.value; // Obtenemos el valor seleccionado en el html.
     value = value.toLowerCase(); // Los valores de las opciones pasan a minúsculas para comparar.
     // Iniciamos indicando que si el valor seleccionado en el botón coincide con el texto el arreglo se mantiene igual a todos los datos traidos desde el back.
@@ -57,7 +68,7 @@ export class BarraFiltroComponent {
       this.filteredItems = this.posts;
     } else {
       // En caso de que seleccione otros valores se debe crear un arreglo nuevo con sólo los elementos que llevan ese o esos valores e imprimirlos en el html.
-      this.filteredItems = this.posts.filter(item => {
+      this.filteredItems = this.posts.filter((item) => {
         // Empezamos a evaluar en este caso los servicios que es mi arreglo de elementos que deseo filtrar
         for (const servicio of item.services) {
           if (servicio.toLowerCase() === value) {
@@ -66,10 +77,23 @@ export class BarraFiltroComponent {
         }
         return false; //Aseguramos que luego del for se retorne algo en caso de que ningún elemento coincida
         //Aseguramos un retorno
-      }
-      );
+      });
     }
-  }
+  } //Fin Función applyFilterServices
 
+  //Función para filtrar por medio del botón
+  applyFilterElement(selectedCategory: any) {
+    let value = selectedCategory.target.value; // Obtenemos el valor seleccionado en el html.
+    value = value.toLowerCase(); // Los valores de las opciones pasan a minúsculas para comparar.
+    // Iniciamos indicando que si el valor seleccionado en el botón coincide con el texto el arreglo se mantiene igual a todos los datos traidos desde el back.
+    if (value === 'todos') {
+      this.filteredItems = this.posts;
+    } else {
+      // En caso de que seleccione otros valores se debe crear un arreglo nuevo con sólo los elementos que llevan ese o esos valores e imprimirlos en el html.
+      this.filteredItems = this.posts.filter((item) => {
+        return item.ciudad.toLowerCase() === value;
+      });
+    }
+  } //Fin Función applyFilterServices
 
 }
